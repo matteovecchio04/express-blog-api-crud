1746f2fae751f5ab0c120377e942979d944605c8
matteovecchio04 <matteovex@gmail.com>
2025-12-03 02:43:43 +0100

    index gives back post list in json and show gives back a single post in json

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,19 @@
 let posts = require("../data/posts");
 
 function index(req, res) {
-    res.send("lista dei post");
+    res.json(posts);
 }
+// gives back the whole array from (posts)
 
 function show(req, res) {
-    res.send("singolo post");
+    const id = Number(req.params.id)
+    const post = posts.find(p => p.id === id)
+
+    if (!post) {
+        return res.status(404).json({ error: "ERROR 404 POST NOT FOUND" })
+    }
+
+    res.json(post)
 }
 
 function create(req, res) {
