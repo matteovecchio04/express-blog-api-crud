45b066f17a6341962ad0096817ad4b7792bec815
matteovecchio04 <matteovex@gmail.com>
2025-12-03 18:56:53 +0100

    add middlewares 404 and 500

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.use(express.json())
 const postsRouter = require("./routes/postsRoute")
 app.use("/posts", postsRouter)
 
+// middlewares
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Rotta non esistente" })
+})
+
+app.use((err, req, res, next) => {
+    console.error("Errore del server", err)
+
+    res.status(500).json({ error: "Errore del server" })
+})
+// /middlewares
+
 app.listen(port, () => {
     console.log(`server avviato su http://localhost:${port}`)
 })
